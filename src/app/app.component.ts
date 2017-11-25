4cd653bdabfdcdfea5ab4020e091376363cff57f
brandon resheske <bresheske@gmail.com>
2017-11-25 14:32:53 -0600

    new format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,6 @@ export class AppComponent {
   }
 
   ngOnInit() {
-    
+    window['Reveal'].reveal('.row', 10);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,54 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { CurrentWorkComponent } from './current-work/current-work.component';
-import { EducationComponent } from './education/education.component';
-import { WritingsComponent } from './writings/writings.component';
-import { SmallBoxComponent } from './small-box/small-box.component';
-import { SideProjectsComponent } from './side-projects/side-projects.component';
-import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
-import { AddressBoxComponent } from './address-box/address-box.component';
-import { SmallHeaderBoxComponent } from './small-header-box/small-header-box.component';
-import { WritingAngularAnimationsComponent } from './writing-angular-animations/writing-angular-animations.component';
-import { WritingAgileComponent } from './writing-agile/writing-agile.component';
-import { WritingRoutingGithubPagesComponent } from './writing-routing-github-pages/writing-routing-github-pages.component';
-
-const appRoutes: Routes = [
-  { path: '', redirectTo: '/main', pathMatch: 'full' },
-  { path: 'main', component: MainComponent },
-  { path: 'current-work', component: CurrentWorkComponent },
-  { path: 'education', component: EducationComponent },
-  { path: 'writings', component: WritingsComponent },
-  { path: 'side-projects', component: SideProjectsComponent },
-  { path: 'writings/angular-route-animation', component: WritingAngularAnimationsComponent },
-  { path: 'writings/agile-thoughts', component: WritingAgileComponent },
-  { path: 'writings/angular-routing-github-pages', component: WritingRoutingGithubPagesComponent }
-];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    CurrentWorkComponent,
-    EducationComponent,
-    WritingsComponent,
-    SmallBoxComponent,
-    SideProjectsComponent,
-    BreadcrumbComponent,
-    AddressBoxComponent,
-    SmallHeaderBoxComponent,
-    WritingAngularAnimationsComponent,
-    WritingAgileComponent,
-    WritingRoutingGithubPagesComponent
   ],
   imports: [
     BrowserAnimationsModule,
-    RouterModule.forRoot(
-      appRoutes,  {}),
     BrowserModule
   ],
   providers: [],
