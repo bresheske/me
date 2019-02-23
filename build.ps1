9c08895d4856f07b3248d56471aef45477558e73
brandon <bresheske@gmail.com>
2019-02-23 09:24:25 -0600

    finally deprecated the ps1 deployer

--- build.ps1
+++
@@ -1,9 +0,0 @@
-npm run ng -- build --prod
-copy-item "dist\index.html" -destination "dist\404.html"
-set-location "dist"
-git init
-git remote add origin https://github.com/bresheske/bresheske.github.io.git
-git add -A
-git stage *
-git commit -m "commit from build.ps1"
-git push --set-upstream origin master --force
